9028b19c7f5cea64fbe87d4db31cd4f008cec04a
raniabns <bensalemrania0@gmail.com>
2024-04-03 01:03:56 +0100

    "update"

--- ApiSide/microreservation/src/test/java/com/rania/microreservation/MicroReservationApplicationTests.java
+++ ApiSide/microreservation/src/test/java/com/rania/microreservation/MicroReservationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroProductApplicationTests {
+class MicroReservationApplicationTests {
 
 	@Test
 	void contextLoads() {
