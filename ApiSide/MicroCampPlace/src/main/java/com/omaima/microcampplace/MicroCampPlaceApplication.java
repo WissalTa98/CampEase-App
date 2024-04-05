99723a77101c651ee4887e2e4284ac48584382e2
OumaymaBaratli <omaima.baratli@esprit.tn>
2024-04-05 01:11:01 +0100

    "AJOUT CRUD + DOCKERFILE"

---
+++ ApiSide/MicroCampPlace/src/main/java/com/omaima/microcampplace/MicroCampPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.omaima.microcampplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCampPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCampPlaceApplication.class, args);
+	}
+
+}
