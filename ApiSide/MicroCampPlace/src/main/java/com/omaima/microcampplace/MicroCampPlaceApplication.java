3d6086ba3949c307ddfb2df814c36a7eb1ab4a13
OumaymaBaratli <omaima.baratli@esprit.tn>
2024-04-10 22:17:21 +0100

    Adding Eureka & Dockerfile

--- ApiSide/MicroCampPlace/src/main/java/com/omaima/microcampplace/MicroCampPlaceApplication.java
+++ ApiSide/MicroCampPlace/src/main/java/com/omaima/microcampplace/MicroCampPlaceApplication.java
@@ -2,8 +2,11 @@ package com.omaima.microcampplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+
 public class MicroCampPlaceApplication {
 
 	public static void main(String[] args) {
