2fdeaff24cb9eeb256debe6303cb92b1895fa0a4
souhail-ftw <souhaildahmeni8@gmail.com>
2024-04-12 00:09:30 +0200

    integrating advanced microservice

--- ApiSide/microAuth/env/Scripts/activate.bat
+++ ApiSide/microAuth/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Wissal Talbi\Desktop\Auth\CampEase-App\ApiSide\microAuth\env
+set VIRTUAL_ENV=C:\Users\Souhaiel\CampEase-App\ApiSide\microAuth\env
 
 if not defined PROMPT set PROMPT=$P$G
 
