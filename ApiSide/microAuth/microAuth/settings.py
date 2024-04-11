1a98a702ba262749718b787896a2b6d08150b96a
WissalTa98 <wissal.talbi@esprit.tn>
2024-04-12 01:16:19 +0200

    Migrate to postgresql

--- ApiSide/microAuth/microAuth/settings.py
+++ ApiSide/microAuth/microAuth/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'microAuth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'authmicro',
+        'USER': 'wisal',
+        'PASSWORD': 'wissal12',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
