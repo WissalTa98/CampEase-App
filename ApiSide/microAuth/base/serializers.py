72b24bfcbc5f7103b7f66e6edadd8a7e4478a43f
WissalTa98 <wissal.talbi@esprit.tn>
2024-04-11 22:49:26 +0200

    Fix issue

--- ApiSide/microAuth/base/serializers.py
+++ ApiSide/microAuth/base/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
+        fields = ['id', 'first_name', 'last_name', 'email', 'password', 'username']
         extra_kwargs = {
             'password': {'write_only': True}
         }
--- ApiSide/microAuth/base/views.py
+++ ApiSide/microAuth/base/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
 from .models import *
+from rest_framework.exceptions import AuthenticationFailed
+import jwt, datetime
 
 
 # Create your views here.
@@ -13,3 +14,32 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
